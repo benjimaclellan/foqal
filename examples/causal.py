14e590a8df51e3008fb23706da5dc0ab68978af3
Benjamin MacLellan <bmaclell@uwaterloo.ca>
2022-07-04 12:38:32 -0400

    Add quantum common cause model

--- examples/causal.py
+++ examples/causal.py
@@ -3,12 +3,14 @@ import itertools
 import tqdm
 import pandas as pd
 import torch
+from torch.functional import F
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 from foqal.utils.io import IO
 from foqal.causal.classical import ClassicalCommonCause, Superdeterminism, Superluminal
+from foqal.causal.quantum import QuantumCommonCause
 from foqal.fit import fit
 
 
@@ -20,36 +22,43 @@ if __name__ == "__main__":
     device = torch.cuda.current_device()
 
     io = IO.directory(
-        folder="entangled-state-data", include_date=False, include_uuid=False
+        folder="entangled-state-data", include_date=False, include_uuid=False, verbose=False,
     )
-    ms = (5, 10, 15, 20, 25, 30, 40, 50, 60, 70, 80, 90, 100)
-    ps = (0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0)
-    runs = (0, 1, 2)
+    
+    # ms = (5, 10, 15, 20, 25, 30, 40, 50, 60, 70, 80, 90, 100)
+    ps = (0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0,)
+    ms = (70,)
 
-    latent_dim = 100
+    latent_dim = 30
     lr = 0.05
-    n_steps = 400
+    n_steps = 300
 
-    q = list(itertools.product(ms, ps, runs))
+    q = list(itertools.product(ms, ps))
 
     verbose, show = False, False
     df = []
     for i in (pbar := tqdm.tqdm(range(len(q)))):
-        (m, p, run) = q[i]
+        (m, p) = q[i]
+        pbar.set_description(f"m={m} | p={p}")
 
-        # load data
-        data = torch.Tensor(io.load_np_array(filename=f"m={m}_p={p}_run{run}.npy"))
+        train_data = torch.Tensor(io.load_np_array(filename=f"m={m}_p={p}_run{0}.npy"))
+        test_data = torch.Tensor(io.load_np_array(filename=f"m={m}_p={p}_run{1}.npy"))
         if use_device:
-            data = data.to(device)
-
-        training_curves = {}
+            train_data = train_data.to(device)
+            test_data = test_data.to(device)
 
         for Model in [
             ClassicalCommonCause,
             Superdeterminism,
             Superluminal,
+            QuantumCommonCause,
         ]:
-            model = Model(num_settings=m, latent_dim=latent_dim)
+            if Model is QuantumCommonCause:
+                _latent_dim = 2
+            else:
+                _latent_dim = latent_dim
+
+            model = Model(num_settings=m, latent_dim=_latent_dim)
 
             if use_device:
                 model = model.to(device)
@@ -57,10 +66,8 @@ if __name__ == "__main__":
             optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
             t0 = time.time()
-            losses = fit(model, data, optimizer, n_steps=n_steps, progress=False)
+            losses = fit(model, train_data, optimizer, n_steps=n_steps, progress=False)
             t1 = time.time()
-
-            training_curves[model.__class__.__name__] = losses
 
             if verbose:
                 print(
@@ -72,24 +79,25 @@ if __name__ == "__main__":
 
             torch.cuda.empty_cache()
 
-        df.append(dict(
-            model=model.__class__.__name__,
-            m=m,
-            p=p,
-            run=run,
-            latent_dim=latent_dim,
-            t=(t1 - t0),
-            lr=lr,
-            n_steps=n_steps,
-        ))
+            loss_test = F.mse_loss(model.forward(), test_data)
+            if loss_test.is_cuda:
+                loss_test = loss_test.cpu().detach().numpy().item()
+            else:
+                loss_test = loss_test.detach().numpy().item()
 
-        if show:  # plot fitting curves
-            fig, ax = plt.subplots(1, 1)
-            for label, losses in training_curves.items():
-                ax.plot(np.arange(len(losses)), np.log(losses), label=f"{label}")
+            df.append(dict(
+                model=model.__class__.__name__,
+                m=m,
+                p=p,
+                latent_dim=_latent_dim,
+                train_loss=losses[-1].item(),
+                test_loss=loss_test,
+                # train_curve=losses,
+                t=(t1 - t0),
+                lr=lr,
+                n_steps=n_steps,
+            ))
 
-            ax.legend()
-            plt.show()
-
+    io.verbose = True
     df = pd.DataFrame(df)
     io.save_dataframe(df, filename="summary_of_fitting.txt")
---
+++ foqal/causal/quantum.py
@@ -0,0 +1,145 @@
+import torch
+from torch import nn
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+
+from foqal.model import ModelBase
+from foqal.utils.io import IO
+from foqal.fit import fit
+
+
+class QuantumCommonCause(ModelBase):
+    def __init__(self, num_settings: int, latent_dim: int, **kwargs):
+        super().__init__(**kwargs)
+
+        self.num_settings = num_settings
+
+        if latent_dim > 4:
+            raise UserWarning(
+                "Are you sure you want a quantum causal model latent dimension greater than 4?"
+            )
+        if latent_dim > 8:
+            raise RuntimeError(
+                "A quantum causal model with latent dimension greater than 8 is not supported yet. "
+            )
+
+        self.latent_dim = latent_dim
+
+        self.terms = {
+            "E(X=0|S)": dict(
+                shape=(self.num_settings, self.latent_dim, self.latent_dim),
+                bounds=(-1, 1),
+            ),
+            "E(Y=0|T)": dict(
+                shape=(self.num_settings, self.latent_dim, self.latent_dim),
+                bounds=(-1, 1),
+            ),
+            "rho(AB)": dict(
+                shape=(1, self.latent_dim**2, self.latent_dim**2),
+                bounds=(-1, 1),
+            ),
+        }
+
+        self.initialize_params()
+        self.identity = nn.Parameter(torch.eye(self.latent_dim)[None, :, :], requires_grad=False)
+        return
+
+    @staticmethod
+    def density_operators(param):
+        # parameterize the T-matrix (dims 1,2) for each setting (dim 0)
+        t_re = torch.tril(param, diagonal=0)
+        t_im = torch.transpose(torch.triu(param, diagonal=1), -2, -1)
+        t = t_re + 1j * t_im
+
+        # generate a positive, semi-definite, hermitian operator with unit trace for each setting
+        tt = torch.conj(torch.transpose(t, -2, -1)) @ t
+        norm = torch.einsum("ijj", tt)[:, None, None]
+        rho = tt / norm  # normalize by the trace along dims (1,2)
+        return rho
+
+    def forward(self):
+        # self.clip_params()
+
+        ex = self.density_operators(self.params["E(X=0|S)"])
+        ey = self.density_operators(self.params["E(Y=0|T)"])
+
+        t = {
+            "E(X|S)": torch.stack([ex, self.identity - ex], dim=0),
+            "E(Y|T)": torch.stack([ey, self.identity - ey], dim=0),
+            "rho(AB)": self.density_operators(self.params["rho(AB)"]),
+        }
+        # print(t)
+        # print("E(X|S) shape", t["E(X|S)"].shape)
+        out_shape = [
+            2,
+            2,
+            self.num_settings,
+            self.num_settings,
+            self.latent_dim**2,
+            self.latent_dim**2,
+        ]
+
+        # Kronecker product of all measurement outcomes, XY
+        e = torch.reshape(torch.einsum("xsik,ytjl->xystijkl", t["E(X|S)"], t["E(Y|T)"]), shape=out_shape)
+
+        # matrix multiplication of [E(X|S) \otimes E(Y|T)] @ rho(AB)
+        out = torch.einsum("xystij,qjk->xystik", e, t["rho(AB)"])
+
+        pred = torch.einsum("xystii->xyst", out)
+        return torch.real(pred)
+
+
+if __name__ == "__main__":
+    use_device = True
+
+    print(f"CUDA is available: {torch.cuda.is_available()}")
+    print(f"Number of devices: {torch.cuda.device_count()}")
+    print(f"Current device: {torch.cuda.get_device_name(0)}")
+    device = torch.cuda.current_device()
+
+    io = IO.directory(
+        folder="entangled-state-data", include_date=False, include_uuid=False
+    )
+
+    run = 0
+    m = 30
+    p = 0.0
+    latent_dim = 2
+
+    data = torch.Tensor(io.load_np_array(filename=f"m={m}_p={p}_run{run}.npy"))
+    if use_device:
+        data = data.to(device)
+
+    training_curves = {}
+
+    model = QuantumCommonCause(num_settings=m, latent_dim=latent_dim)
+
+    if use_device:
+        model = model.to(device)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.15)
+
+    t0 = time.time()
+    losses = fit(model, data, optimizer, n_steps=400)
+
+    training_curves[model.__class__.__name__] = losses
+
+    print(
+        f"\n{model.__class__.__name__} | "
+        f"\n\tTotal time: {time.time() - t0}| "
+        f"\n\tTotal parameters: {sum(p.numel() for p in model.parameters())}"
+        f"\n\tFinal loss: {losses[-1]}"
+    )
+
+    torch.cuda.empty_cache()
+
+    fig, ax = plt.subplots(1, 1)
+    for label, losses in training_curves.items():
+        ax.plot(np.arange(len(losses)), np.log(losses), label=f"{label}")
+
+    ax.legend()
+    plt.show()
+
+    pred = model.forward()
+    print(torch.sum(pred, (0, 1)))
