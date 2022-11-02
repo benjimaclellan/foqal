Fix nan issue with KL divergence when p=0

experiments/causal/regression.py
@@ -3,7 +3,6 @@ import itertools
 import tqdm
 import pandas as pd
 import torch
-import numpy as np
 
 from foqal import optim
 from foqal.utils.io import IO
@@ -60,22 +59,30 @@ if __name__ == "__main__":
             if Model is QuantumCommonCause:
                 _latent_dim = 2
             else:
-                _latent_dim = max([100, m])
+                _latent_dim = max([15, m])
 
-            model = Model(n_settings=m, latent_dim=_latent_dim)
-            model = model.to(device)
+            check = True
+            while check:
 
-            optimizer = torch.optim.Adagrad(model.parameters(), lr=lr)
-            loss = optim.KLDivLoss()
+                model = Model(n_settings=m, latent_dim=_latent_dim)
+                model = model.to(device)
 
-            t0 = time.time()
-            losses = fit(
-                model, train_data, optimizer, loss, n_steps=n_steps, progress=False
-            )
-            t1 = time.time()
+                optimizer = torch.optim.Adagrad(model.parameters(), lr=lr)
+                loss = optim.KLDivLoss()
 
-            loss_train = to_numpy(loss(model.forward(), train_data))
-            loss_test = to_numpy(loss(model.forward(), test_data))
+                t0 = time.time()
+                losses = fit(
+                    model, train_data, optimizer, loss, n_steps=n_steps, progress=False
+                )
+                t1 = time.time()
+
+                loss_train = to_numpy(loss(model.forward(), train_data))
+                loss_test = to_numpy(loss(model.forward(), test_data))
+
+                if (Model == QuantumCommonCause) & loss_train > 0.0002:
+                    check = True
+                else:
+                    check = False
 
             df.append(
                 dict(
@@ -97,11 +104,11 @@ if __name__ == "__main__":
         if i % len(ps):
             io.verbose = True
             io.save_dataframe(pd.DataFrame(df), filename="results/regression_summary.txt")
-            io.save_np_array(np.array(curves), filename="results/training_curves")
+            io.save_json(curves, filename="results/training_curves")
             io.verbose = False
 
     df = pd.DataFrame(df)
     io.verbose = True
     io.save_dataframe(df, filename="results/regression_summary.txt")
-    io.save_np_array(np.array(curves), filename="results/training_curves")
+    io.save_json(curves, filename="results/training_curves")
     print("Regression finished.")

experiments/causal/sample.py
@@ -2,7 +2,6 @@ import qutip as qt
 import numpy as np
 import ray
 import itertools
-import psutil
 
 from foqal.utils.io import IO
 from foqal.utils.sample import sample_bloch_vectors, bloch_vectors_to_kets
@@ -15,6 +14,7 @@ def simulate_local_projective_measurements(
     m: int = 10,
     method: str = "fibonnaci",
     total_counts=2000,
+    background=0,
 ):
     """
     Simulates two-qubit measurement outcome probabilities for pairs of local, sampled projective measurements.
@@ -26,6 +26,7 @@ def simulate_local_projective_measurements(
     :param m: number of unique projective measurements applied locally on each qubit
     :param method: sampling method. options are "haar", "fibonacci"
     :param total_counts: total number of measurement counts (i.e., number of photon events)
+    :param background: number of background events (as p=0.00 can cause numerical issues)
     :return:
     """
     assert method in ("fibonnaci", "haar")
@@ -46,7 +47,7 @@ def simulate_local_projective_measurements(
 
     datasets = []
     for j in range(n_datasets):
-        _data = np.random.poisson(total_counts * data)
+        _data = np.random.poisson(total_counts * data) + background
         _data = _data / np.sum(_data, axis=(0, 1))[None, None, :, :]
         datasets.append(_data)
     return datasets
@@ -63,6 +64,7 @@ if __name__ == "__main__":
     n_datasets = 2
     ms = (5, 10, 15, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200)
     ps = (0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0)
+    background = 1
 
     @ray.remote
     def sample_data(m, p, n_datasets):
@@ -72,6 +74,7 @@ if __name__ == "__main__":
             n_datasets=n_datasets,
             m=m,
             method="fibonnaci",
+            background=background
         )
 
         for k, data in enumerate(datasets):
@@ -79,13 +82,13 @@ if __name__ == "__main__":
 
             io.save_np_array(
                 data.astype("float"),
-                filename=f"num_states={m}_p={int(100 * p)}_{k}",
+                filename=f"m={m}_p={int(100 * p)}_{k}",
             )
         return
 
 
-    ray.init(num_cpus=psutil.cpu_count()-1, ignore_reinit_error=True)
+    ray.init(num_cpus=24, ignore_reinit_error=True)
     futures = [sample_data.remote(m, p, n_datasets) for (m, p) in itertools.product(ms, ps)]
     ray.get(futures)
 
-    print("Complete")
+    print("Simulating data complete.")